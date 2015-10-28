8d890cb5b3eee77d0e6227ba1cb4ab7390918912
jpvandy <jpvandy@sandia.gov>
2015-10-28 08:27:35 -0600

    make TimeLimit work with python wrapped sst

--- test/utilities/timeLimitEnforcer.sh
+++ test/utilities/timeLimitEnforcer.sh
@@ -19,6 +19,20 @@ echo ' '
 
 SSTX_PID=`ps -ef | grep 'sst ' | grep $PPID | awk '{ print $2 }'`
 
+if [ -z $SSTX_PID ] ; then
+    echo I am $MY_PID,  I was called from $1, my parent PID is $PPID
+    echo "No corresponding child named \"sst\" "
+    ps -f | grep $PPID
+    echo ' '
+    #   Is there a Python running from the Parent PID
+    echo " Look for a child named \"python\""
+    PY_PID=`ps -ef | grep 'python ' | grep $PPID | awk '{ print $2 }'`
+    if [ -z $PY_PID ] ; then
+        echo "No corresponding child named \"python\" "
+        echo ' '
+    fi
+    SSTX_PID=`ps -ef | grep 'sst ' | grep $PY_PID | awk '{ print $2 }'`
+fi
 ps -f -p $SSTX_PID | grep $SSTX_PID
 
 if [ $? == 1 ] ; then
@@ -33,7 +47,6 @@ else
         echo " Kill of $SST_PIDX for TIME OUT   FAILED"
         echo "     I am $MY_PID,   my parent was $PPID" 
         ps -f U $USER
-    else
         echo " Try a \"kill -9\"  "
         kill -9 $SSTX_PID
     fi
