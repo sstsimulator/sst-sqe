0e2a379fe85e07ce0e765959b47acbc1d6881427
John <jpvandy@users.noreply.github.com>
2016-03-07 13:30:58 -0700

    Update testSuite_EmberSweep.sh

--- test/testSuites/testSuite_EmberSweep.sh
+++ test/testSuites/testSuite_EmberSweep.sh
@@ -179,7 +179,7 @@ SE_fini() {
     if [[ ${SST_MULTI_THREAD_COUNT:+isSet} != isSet ]] ; then
         cp  ${SST_TEST_INPUTS}/EmberSweepGenerator.py .
     else
-        sed '/print..sst.*model/s/sst./sst -n '"${SST_MULTI_THREAD_COUNT} /" test/testInputFiles/EmberSweepGenerator.py > EmberSweepGenerator.py
+        sed '/print..sst.*model/s/sst./sst -n '"${SST_MULTI_THREAD_COUNT} /" ${SST_TEST_INPUTS}/EmberSweepGenerator.py > EmberSweepGenerator.py
         chmod +x EmberSweepGenerator.py
     fi
     ./EmberSweepGenerator.py > bashIN
