e1601461e76056e10bc48d66c1722e06bb1108be
Aaron Levine <allevin@sandia.gov>
2016-04-13 13:20:05 -0600

    Fix HybridSim for empty string params - Causing core exception
    String params must not have empty strings, causes exception in core
    with new param code.

--- test/testInputFiles/testSdlFiles/HybridSim/sdl3-2-edit.py
+++ test/testInputFiles/testSdlFiles/HybridSim/sdl3-2-edit.py
@@ -25,7 +25,7 @@ comp_c0_l1cache.addParams({
       "cache_size" : """4 KB""",
       "printStats" : """1""",
       "L1" : """1""",
-      "debug" : """"""
+      "debug" : """0"""
 })
 comp_cpu1 = sst.Component("cpu1", "memHierarchy.trivialCPU")
 comp_cpu1.addParams({
@@ -45,7 +45,7 @@ comp_c1_l1cache.addParams({
       "cache_size" : """4 KB""",
       "printStats" : """1""",
       "L1" : """1""",
-      "debug" : """"""
+      "debug" : """0"""
 })
 comp_bus = sst.Component("bus", "memHierarchy.Bus")
 comp_bus.addParams({
@@ -62,13 +62,13 @@ comp_l2cache.addParams({
       "cache_size" : """32 KB""",
       "printStats" : """1""",
       "L1" : """0""",
-      "debug" : """""",
+      "debug" : """0""",
       "mshr_num_entries" : """4096"""
 })
 comp_memory = sst.Component("memory", "memHierarchy.MemController")
 comp_memory.addParams({
       "coherence_protocol" : """MSI""",
-      "debug" : """""",
+      "debug" : """0""",
       "system_ini" : os.environ['SST_DEPS_INSTALL_HYBRIDSIM'] + '/ini/hybridsim.ini',
       "clock" : """1GHz""",
       "access_time" : """1000 ns""",
