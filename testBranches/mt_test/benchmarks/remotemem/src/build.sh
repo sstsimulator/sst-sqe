b5c8735c894718d308fd52ddde5e8e9ba489c52d
jpvandy <jpvandy@sandia.gov>
2016-07-12 08:58:21 -0600

    Remove unused/mislocated sub-trees

--- testBranches/mt_test/benchmarks/remotemem/src/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ `uname` = "Darwin" ]; then
-	clang++ -O3 -o lulesh lulesh-OPTIM-OMP-ALLOC-VEC3-AGA.cc
-else
-	g++ -o lulesh -O3 lulesh-OPTIM-OMP-ALLOC-VEC3-AGA.cc
-fi
--- testBranches/test_5.1.0_pre/benchmarks/remotemem/src/miniomp.h
+++
@@ -1,3 +0,0 @@
-
-#define omp_get_max_threads() 1
-#define omp_get_thread_num() 0
