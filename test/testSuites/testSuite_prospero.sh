f425767b7226bee1a38f9cb6ebef40d78fd53af4
John <jpvandy@users.noreply.github.com>
2016-07-21 08:09:36 -0600

    More bad syntax

--- test/testSuites/testSuite_prospero.sh
+++ test/testSuites/testSuite_prospero.sh
@@ -33,13 +33,13 @@ L_BUILDTYPE=$1 # Build type, passed in from bamboo.sh as a convenience
 
 L_TESTFILE=()  # Empty list, used to hold test file names
 echo "NEW  ${SST_BUILD_PROSPERO_TRACE_FILE}"
-env | grep $SST_TEST_HOST_OS 
+env | grep SST_TEST_HOST_OS 
 if [[ ${SST_BUILD_PROSPERO_TRACE_FILE:+isSet} == isSet ]] ; then
 echo line 38  $LINENO
     if [[ $SST_TEST_HOST_OS_DISTRIB_VERSION == "10.10" ]] ; then
       preFail "Prospero Pin does not work on Yosemite - July 2016"  "skip"
     fi  
-    fail " NOT supposed to get here"
+    preFail " NOT supposed to get here"
    # ==================  Create program "array"
 
    # ----------------- compile the file array     
