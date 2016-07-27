f6b268ca0e36871be3f0c96bc7a4018e3e13e9d3
Aaron Levine <allevin@sandia.gov>
2016-07-27 10:47:17 -0600

    Final changes to init code

--- buildsys/bamboo.sh
+++ buildsys/bamboo.sh
@@ -1265,18 +1265,18 @@ linuxSetBoostMPI() {
 
    echo "Attempt to initialize the modules utility.  Look for modules init file in 1 of 2 places"
    
-   echo "Location 1: ls -l /etc/profile.modules.sh"
-   ls -l /etc/profile.modules.sh
+   echo "Location 1: ls -l /etc/profile.modules"
+   ls -l /etc/profile.modules
    if [ -f /etc/profile.modules ] ; then
        . /etc/profile.modules
-       echo "bamboo.sh: loaded /etc/profile.modules."
-   fi
-   
-   echo "Location 2: ls -l /etc/profile.d/modules.sh"
-   ls -l /etc/profile.d/modules.sh
-   if [ -r /etc/profile.d/modules.sh ] ; then 
-       source /etc/profile.d/modules.sh 
-       echo "bamboo.sh: loaded /etc/profile.d/modules."
+       echo "bamboo.sh: loaded /etc/profile.modules"
+   else
+       echo "Location 2: ls -l /etc/profile.d/modules.sh"
+       ls -l /etc/profile.d/modules.sh
+       if [ -r /etc/profile.d/modules.sh ] ; then 
+           source /etc/profile.d/modules.sh 
+           echo "bamboo.sh: loaded /etc/profile.d/modules"
+       fi
    fi
    
    echo "Testing modules utility via ModuleEx..."
