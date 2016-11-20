1246ac67de2ddd56b09da6210fc7da642f10a851
John <jpvandy@users.noreply.github.com>
2016-11-19 19:05:42 -0700

    Update testSuite_CramSim.sh

--- test/testSuites/testSuite_CramSim.sh
+++ test/testSuites/testSuite_CramSim.sh
@@ -17,10 +17,6 @@ TEST_SUITE_ROOT="$( cd -P "$( dirname "$0" )" && pwd )"
 . $TEST_SUITE_ROOT/../include/testSubroutines.sh
 
 
-if [[ $SST_BUILD_TYPE == "sstmainline_config_no_gem5" ]] ; then
-     preFail " CramSim has problems with make-dist"  "skip"
-fi
-
 #=============================================================================
 # Variables global to functions in this suite
 #===============================================================================
