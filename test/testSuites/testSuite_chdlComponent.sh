699a416a46a9e4af5b459fc2f59fe23d3f1e0d1c
jpvandy <jpvandy@sandia.gov>
2017-01-26 11:43:43 -0700

    Last two(?) sqe conversion to use moved ref files

--- test/testSuites/testSuite_chdlComponent.sh
+++ test/testSuites/testSuite_chdlComponent.sh
@@ -96,6 +96,7 @@ test_chdlComponent() {
     testDataFileBase="test_chdlComponent"
     outFile="${SST_TEST_OUTPUTS}/${testDataFileBase}.out"
     referenceFile="${SST_TEST_REFERENCE}/${testDataFileBase}.out"
+    referenceFile="${SST_REFERENCE_ELEMENTS}/chdl/tests/refFiles/${testDataFileBase}.out"
     # Add basename to list for XML processing later
     L_TESTFILE+=(${testDataFileBase})
 
