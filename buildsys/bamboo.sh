05831416c7737bdb8d13e8dd39347990460dce8c
John <jpvandy@users.noreply.github.com>
2017-02-17 12:36:04 -0700

    Update bamboo.sh

--- buildsys/bamboo.sh
+++ buildsys/bamboo.sh
@@ -500,7 +500,7 @@ echo " #####################################################"
         ${SST_TEST_SUITES}/testSuite_BadPort.sh
         export SST_MULTI_THREAD_COUNT=2
         export SST_TEST_SE_LIST="1 9 151"
-        HAM=25
+        HAM=5
         while [ $HAM -gt 0 ]
         do
    echo $HAM
