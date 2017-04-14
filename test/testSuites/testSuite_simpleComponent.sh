206ebd6703e7e58d39451521c42b80bd334fa3b8
jpvandy <jpvandy@sandia.gov>
2017-04-14 12:02:34 -0600

    Remove untemplated SubComponent test

--- test/testSuites/testSuite_simpleComponent.sh
+++ test/testSuites/testSuite_simpleComponent.sh
@@ -6,10 +6,6 @@
 # A shell script that defines a shunit2 test suite. This will be
 # invoked by the Bamboo script.
 
-# Preconditions:
-
-# 1) The SUT (software under test) must have built successfully.
-# 2) A test success reference file is available.
 
 TEST_SUITE_ROOT="$( cd -P "$( dirname "$0" )" && pwd )"
 # Load test definitions
@@ -136,74 +132,6 @@ simpleComponent_Template  SubComponent
 
 }
 
-test_simpleSubComponent_original() {
-
-    # Define a common basename for test output and reference
-    # files. XML postprocessing requires this.
-    testDataFileBase="test_simpleSubComponent"
-    outFile="${SST_TEST_OUTPUTS}/${testDataFileBase}.out"
-    testOutFiles="${SST_TEST_OUTPUTS}/${testDataFileBase}.testFile"
-    referenceFile="${SST_REFERENCE_ELEMENTS}/simpleElementExample/tests/refFiles/${testDataFileBase}.out"
-    # Add basename to list for XML processing later
-    L_TESTFILE+=(${testDataFileBase})
-
-    # Define Software Under Test (SUT) and its runtime arguments
-    sut="${SST_TEST_INSTALL_BIN}/sst"
-    sutArgs="${SST_ROOT}/sst-elements/src/sst/elements/simpleElementExample/tests/test_simpleSubComponent.py"
-
-    if [ -f ${sut} ] && [ -x ${sut} ]
-    then
-        # Run SUT
-        if [[ ${SST_MULTI_RANK_COUNT:+isSet} != isSet ]] ; then
-           ${sut} ${sutArgs} > ${outFile}
-           RetVal=$? 
-        else
-           mpirun -np ${SST_MULTI_RANK_COUNT} -output-filename $testOutFiles -map-by numa:PE=2 ${sut} ${sutArgs}
-           RetVal=$? 
-           cat ${testOutFiles}* > $outFile
-        fi
-
-        TIME_FLAG=/tmp/TimeFlag_$$_${__timerChild} 
-        if [ -e $TIME_FLAG ] ; then 
-             echo " Time Limit detected at `cat $TIME_FLAG` seconds" 
-             fail " Time Limit detected at `cat $TIME_FLAG` seconds" 
-             rm $TIME_FLAG 
-             return 
-        fi 
-        if [ $RetVal != 0 ]  
-        then
-             echo ' '; echo WARNING: sst did not finish normally ; echo ' '
-             ls -l ${sut}
-             fail "WARNING: sst did not finish normally, RetVal=$RetVal"
-             return
-        fi
-
-        RemoveComponentWarning
-
-        wc $referenceFile $outFile
-        diff -b $referenceFile $outFile > ${SSTTESTTEMPFILES}/_raw_diff
-        if [ $? != 0 ]
-        then  
-           wc ${SSTTESTTEMPFILES}/_raw_diff
-           compare_sorted $referenceFile $outFile
-           if [ $? == 0 ] ; then
-              echo " Sorted match with Reference File"
-              rm ${SSTTESTTEMPFILES}/_raw_diff
-              return
-           else
-              fail " Reference does not Match Output"
-              diff -b $referenceFile $outFile 
-           fi
-        else
-           echo "Exact match with Reference File"
-        fi
-    else
-        # Problem encountered: can't find or can't run SUT (doesn't
-        # really do anything in Phase I)
-        ls -l ${sut}
-        fail "Problem with SUT: ${sut}"
-    fi
-}
 
 export SHUNIT_DISABLE_DIFFTOXML=1
 export SHUNIT_OUTPUTDIR=$SST_TEST_RESULTS
