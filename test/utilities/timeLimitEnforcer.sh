5be821a7270740190adfd5435c3b83fab617786a
jpvandy <jpvandy@sandia.gov>
2017-05-25 14:33:04 -0600

    multiple represention of sst

--- test/utilities/timeLimitEnforcer.sh
+++ test/utilities/timeLimitEnforcer.sh
@@ -77,6 +77,13 @@ findChild()
    KILL_PID=`ps -ef | grep 'sst ' | grep $SPID | awk '{ print $2 }'`
    
    if [ -z "$KILL_PID" ] ; then
+echo "------------------   Debug ------/$SPID is $SPID -------"
+   ps -ef | grep bin/sst
+echo "------------------   Debug -------------"
+       KILL_PID=`ps -ef | grep bin/sst  | grep $SPID | awk '{ print $2 }'`
+   fi
+      
+   if [ -z "$KILL_PID" ] ; then
        echo I am $TL_MY_PID,  findChild invoked with $1, my parent PID is $TL_PPID
        echo "No corresponding child named \"sst\" "
        ps -f | grep $SPID
