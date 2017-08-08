5fa39a70db386ba24152e72c5a58abcfdf0c2451
jpvandy <jpvandy@sandia.gov>
2017-08-08 13:46:42 -0600

    capture output for gwen

--- test/testSuites/testSuite_memHSieve.sh
+++ test/testSuites/testSuite_memHSieve.sh
@@ -126,6 +126,14 @@ test_memHSieve() {
              fail " Time Limit detected at `cat $TIME_FLAG` seconds" 
              rm $TIME_FLAG 
              log_memHS "Time Limit"
+    ls -ltr
+    if [[ `uname -n` == sst-test* ]] ; then
+         __dir=`date | sed 's/ /_/g'`
+         echo "Directory is $__dir"
+         mkdir ~jpvandy/memHS-dir/$__dir
+         cp *map.txt ~jpvandy/memHS-dir/$__dir
+         cp backtrace_*.gz ~jpvandy/memHS-dir/$__dir
+    fi
              return 
         fi 
         if [ $RetVal != 0 ]  
