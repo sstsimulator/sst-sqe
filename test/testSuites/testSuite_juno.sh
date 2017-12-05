b4e0c042fccdb6669381b99c6c210d8ff96f2bed
jpvandy <jpvandy@sandia.gov>
2017-12-05 11:46:58 -0700

    Rewrite Suite to handle multi thread and rank

--- test/testSuites/testSuite_juno.sh
+++ test/testSuites/testSuite_juno.sh
@@ -27,18 +27,33 @@ sst_info_Template() {
    testDataFileBase="juno_${testname_case}"
    outFile="${SST_TEST_OUTPUTS}/${testDataFileBase}.out"
    errFile="${SST_TEST_OUTPUTS}/${testDataFileBase}.err"
-   refFile="${SST_ROOT}/juno/tests/refFiles/${testDataFileBase}.csv"
+   refFile="${SST_ROOT}/juno/tests/refFiles/${testDataFileBase}.out"
 
    cd $SST_ROOT/juno/test/asm           #4
 echo "      -------   done the cd to test/asm "
     ../../asm/sst-juno-asm -i ${testname_case}.juno -o ${testname_case}.bin   #5-6
 echo '    ---  '
-   cd ../sst                                                 #7
+   cd ../sst                                     #7
 
-   JUNO_EXE="../asm/${testname_case}.bin" sst juno-test.py > $outFile 2>$errFile    #8
+##    pyFileName=`echo test${memHA_case}.py | sed s/_/-/`
+
+    sut="${SST_TEST_INSTALL_BIN}/sst"
+    sutArgs=${SST_ROOT}/juno/test/sst/juno-test.py
+
+   export JUNO_EXE="../asm/${testname_case}.bin" 
+   ${sut} ${sutArgs} > $outFile 2>$errFile                  #8
    
    retVal=$?
-echo "Return Code is $retVal"
+   if [ $retVal != 0 ] ; then
+       fail "Return Code is $retVal"
+       echo " ---  Stderr:"
+       cat $errFile
+       echo " ---  Stdout:"
+       cat $outFile
+       echo "   "
+       wc *csv
+       return
+   fi
 
    if [ -s $errFile ] ; then
       cat $errFile
@@ -46,31 +61,39 @@ echo "Return Code is $retVal"
       cat $outFile
       return
    fi
-
-   wc $outFile
-
-  ls -l $refFile output.csv
-   diff $refFile output.csv
-   if [ $? == 0 ] ; then
-      echo " Exact Match"
+   TS=`grep "Simulation is complete" $outFile`
+   if [ $? != 0 ] ; then
+       fail "Did not find completion message"
+       echo " ---  Stderr:"
+       cat $errFile
+       echo " ---  Stdout:"
+       cat $outFile
+       echo "   "
+       wc *csv
+       return
+   fi
+ 
+   RS=`grep "Simulation is complete" $refFile`
+   if [ "$RS" == "$TS" ] ; then
+      echo "    Exact Match"
+      echo "$TS"
    else
       fail " Output did not match Reference"
+      echo "Output: $TS"
+      echo "  Ref:  $RS"
    fi
-
 
 }
 
 
 test_juno_sum() {
 
-echo " --------------- start test invocation "
 sst_info_Template sum
 
 }
 
 test_juno_modulo() {
 
-echo " --------------- start test invocation "
 sst_info_Template modulo
 
 }
