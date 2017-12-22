e62f7e3b5aba2b94853a9de7cd8ad71ecccc22ab
jpvandy <jpvandy@sandia.gov>
2017-12-22 11:01:23 -0700

    Do use Macro test

--- buildsys/bamboo.sh
+++ buildsys/bamboo.sh
@@ -597,6 +597,8 @@ echo " #####################################################"
         return
     fi
 
+        ${SST_TEST_SUITES}/testSuite_macro.sh    ################################################
+
      #
      #   Suites that used MemHierarchy, but not openMP
      #
@@ -665,7 +667,7 @@ echo " #####################################################"
     ${SST_TEST_SUITES}/testSuite_miranda.sh
     ${SST_TEST_SUITES}/testSuite_BadPort.sh
     ${SST_TEST_SUITES}/testSuite_scheduler.sh
-    ${SST_TEST_SUITES}/testSuite_scheduler_DetailedNetwork.sh
+##    ${SST_TEST_SUITES}/testSuite_scheduler_DetailedNetwork.sh
 
     # Add other test suites here, i.e.
     # ${SST_TEST_SUITES}/testSuite_moe.sh
