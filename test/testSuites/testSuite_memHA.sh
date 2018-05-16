f488b414d6ab26fcebc0449effa87170c9715510
jpvandy <jpvandy@sandia.gov>
2018-05-16 12:24:20 -0600

    enable HBM tests

--- test/testSuites/testSuite_memHA.sh
+++ test/testSuites/testSuite_memHA.sh
@@ -68,6 +68,9 @@ Match=$2    ##  Match criteron
         #           This is multi-core case
         if [ -e "$memH_test_dir/refFiles/${testDataFileBase}_MC.out" ] ; then
             referenceFile="$memH_test_dir/refFiles/${testDataFileBase}_MC.out"
+        elif [ -e "$memH_test_dir/refFiles/${testDataFileBase}_MR.out" ] &&
+           [[ ${SST_MULTI_RANK_COUNT:+isSet} == isSet ]] && [ ${SST_MULTI_RANK_COUNT} -gt 1 ] ; then
+            referenceFile="$memH_test_dir/refFiles/${testDataFileBase}_MR.out"
         else
             referenceFile="$memH_test_dir/refFiles/${testDataFileBase}.out"
         fi
