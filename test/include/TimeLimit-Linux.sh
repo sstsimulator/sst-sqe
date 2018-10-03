873faa95b971a7b6e3e8b5f792336472d6e5c6ee
John Van Dyke <jpvandy@users.noreply.github.com>
2018-10-03 22:59:29 +0000

    Update TimeLimit-Linux.sh

--- test/include/TimeLimit-Linux.sh
+++ test/include/TimeLimit-Linux.sh
@@ -38,7 +38,7 @@ echo "$LINENO - ------------------   Debug -------------"
                echo "$LINENO -   at line $LINENO,  kill Valgrind $VG_PID"
                echo "$LINENO - wc from \"ps -fe\" `ps -fe | wc` "
 #              SST_PID=`ps -f | grep 'sstsim.x ' | grep $VG_PID | grep -v $SPID | awk '{ print $3 }'`
-echo "$LINENO John's proof of concept"; echo ' '
+echo "$LINENO John proof of concept"; echo ' '
                        ps -f | grep 'sstsim.x ' | grep $VG_PID | grep -v $SPID | awk '{ print $2 , print $3  } `
      echo "$LINENO \$2 is first"
                    n2=`ps -f | grep 'sstsim.x ' | grep $VG_PID | grep -v $SPID | awk '{ print $2  }' `
