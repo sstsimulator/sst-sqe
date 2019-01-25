1c7a2bfed731dc4424d7030a81a8ceb89574b133
John Van Dyke <jpvandy@users.noreply.github.com>
2019-01-24 17:11:31 -0700

    Update testSuite_Sweep_openMP.sh

--- test/testSuites/testSuite_Sweep_openMP.sh
+++ test/testSuites/testSuite_Sweep_openMP.sh
@@ -313,7 +313,7 @@ Tol=9000    ##  curTick tolerance,  or  "lineWordCt"
         iCASE=$(($iCASE+1))
     else
         INDEX_RUNNING=$(($INDEX_RUNNING+1))
-echo ">>>>>>>>>>>>>>>> INDEX_RUNNING = [$INDEX_RUNNING]"
+## echo ">>>>>>>>>>>>>>>> INDEX_RUNNING = [$INDEX_RUNNING]"
     fi
     echo INDEX_RUNNING is $INDEX_RUNNING
     startSeconds=`date +%s`
@@ -322,7 +322,7 @@ echo ">>>>>>>>>>>>>>>> INDEX_RUNNING = [$INDEX_RUNNING]"
     tmpFile="${SST_TEST_OUTPUTS}/${testDataFileBase}.tmp"
     wrkFile="${SST_TEST_OUTPUTS}/${testDataFileBase}.wrk"
     referenceFile="${SST_TEST_REFERENCE}/${testDataFileBase}.out"
-echo " >>>>>>>>>>>>>>>>>... $outFile "
+## echo " >>>>>>>>>>>>>>>>>... $outFile "
 
     sutArgs=$TEST_SUITE_ROOT/testopenMP/sweepopenmp.py
 
