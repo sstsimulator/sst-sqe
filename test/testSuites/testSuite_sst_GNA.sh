6f99fbb9cfba951a75133f04d4c427bcfd8a2bfe
jpvandy <jpvandy@sandia.gov>
2019-06-27 12:07:58 -0600

    Suite + some inst.

--- test/testSuites/testSuite_sst_GNA.sh
+++ test/testSuites/testSuite_sst_GNA.sh
@@ -4,6 +4,11 @@
 # Description:
 
 # This Suite is minimal test of GNA element
+TEST_SUITE_ROOT="$( cd -P "$( dirname "$0" )" && pwd )"
+# Load test definitions
+. $TEST_SUITE_ROOT/../include/testDefinitions.sh
+. $TEST_SUITE_ROOT/../include/testSubroutines.sh
+
 
 #=============================================================================
 # Variables global to functions in this suite
@@ -14,17 +19,21 @@ L_SUITENAME="SST_sst_GNA" # Name of this test suite;
 L_TESTFILE=()  # Empty list, used to hold test file names
 
 #===============================================================================
+echo " this is b4 template"
 
 template_sst_GNA() {
    testN="test${1}"
    testDataFileBase="test_sst_GNA"
    outFile="${SST_TEST_OUTPUTS}/${testDataFileBase}${testN}.out"
    errFile="${SST_TEST_OUTPUTS}/${testDataFileBase}${testN}.err"
-   refFile=${SST_REFERENCE_ELEMENTS}/GNA/tests/ref/${testN}.out
-
+   refFile=${SST_REFERENCE_ELEMENTS}/GNA/tests/refFiles/${testN}.out
+ls -l $refFile
     sut="${SST_TEST_INSTALL_BIN}/sst"
-    sutArgs=${SST_REFERENCE_ELEMENTS}/GNA/tests/$testN}.py
-   
+    sutArgs=${SST_REFERENCE_ELEMENTS}/GNA/tests/${testN}.py
+ls -l $sutArgs
+
+ echo  ${SST_REFERENCE_ELEMENTS}/GNA/tests
+ls -l ${SST_REFERENCE_ELEMENTS}/GNA/tests
     $sut $sutArgs > $outFile 2>$errFile
 
    if [ -s $errFile ] ; then
@@ -39,23 +48,30 @@ template_sst_GNA() {
        fail " Out Put does not match reference"
    fi
 
+   echo "       Pass  Exact match"
    echo ' '
 
 }
 
-test1( ) {
+test_Sanity_check() {
+echo "this id Duumy test "
+}
+
+test_1() {
+echo  call first
 template_sst_GNA  1
 }
 
 
-test2( ) {
+test_2() {
+echo call second
 template_sst_GNA  2
 }
 export SHUNIT_OUTPUTDIR=$SST_TEST_RESULTS
+
 
 # Invoke shunit2. Any function in this file whose name starts with
 # "test"  will be automatically executed.
 
 (. ${SHUNIT2_SRC}/shunit2)
-
 
