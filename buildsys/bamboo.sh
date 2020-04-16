7cfb9210c569a7ebfa9c0c9324ee5bee8204f2d5
Aaron Levine <allevin@sandia.gov>
2020-04-16 10:06:36 -0600

    Test for none in python version parameter

--- buildsys/bamboo.sh
+++ buildsys/bamboo.sh
@@ -3462,9 +3462,11 @@ else
                 * | none)
                   # Perform a quick check to see if $6 is empty
                   if [ -n "$6" ]; then
-                      echo "ERROR: ILLEGAL PYTHON OPTION " $6
-                      echo "       ONLY python2 | python3 ALLOWED"
-                      exit 128
+                      if [[ $6 != "none" ]] ; then
+                          echo "ERROR: ILLEGAL PYTHON OPTION " $6
+                          echo "       ONLY none | python2 | python3 ALLOWED"
+                          exit 128
+                      fi
                   fi
 
                   # NOTE: THIS CODE IS FOR SST Version 10 - When default search priority is Python first
