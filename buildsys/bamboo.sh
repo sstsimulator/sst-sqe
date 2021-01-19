4097337d3fcd9fa0555742453995923509a561a2
Aaron Levine <allevin@sandia.gov>
2021-01-19 16:00:25 -0700

    First pass at testing

--- buildsys/bamboo.sh
+++ buildsys/bamboo.sh
@@ -3637,6 +3637,21 @@ else
             echo "=============================================================="
 
 
+
+            echo "=============================================================="
+            echo "=== DEBUG CODE FOR SST_TEST_FRAMEWORKS_ELEMENTS_WILDCARD_TESTS"
+            echo "=============================================================="
+            if [[ ${SST_TEST_FRAMEWORKS_ELEMENTS_WILDCARD_TESTS:+isSet} == isSet ]] ; then
+                echo "**************************************************************************"
+                echo "***                                                                    ***"
+                echo "*** RUNING NEW TEST FRAMEWORKS ON WILDCARD SET ${SST_TEST_FRAMEWORKS_ELEMENTS_WILDCARD_TESTS}  ***"
+                echo "***                                                                    ***"
+                echo "**************************************************************************"
+            echo "=============================================================="
+            echo "=== DEBUG DONE"
+            echo "=============================================================="
+
+
        # Figure out PIN Configuration
        if [[  ${SST_WITHOUT_PIN:+isSet} == isSet ]] ; then
             echo "  PIN IS NOT ENABLED BY SST_WITHOUT_PIN flag"
