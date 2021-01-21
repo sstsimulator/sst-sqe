7a296efa8e84b437d411a1db9366bc2523759e41
Aaron Levine <allevin@sandia.gov>
2021-01-21 14:59:45 -0700

    Now turn frameworks back on

--- buildsys/bamboo.sh
+++ buildsys/bamboo.sh
@@ -3878,10 +3878,7 @@ then
                             echo "**************************************************************************"
                             # WE ARE RUNNING THE FRAMEWORKS ELEMENTS SUBSET OF TESTS (Set by wildcard) AFTER DOTESTS() HAVE RUN
                             cd $SST_ROOT
-#DEBUG DISABLE TESTING START
-                            #$SST_PYTHON_APP_EXE $SST_TEST_FRAMEWORKS_ELEMENTS_APP_EXE -z -d -r $SST_MULTI_RANK_COUNT -t $SST_MULTI_THREAD_COUNT -w $SST_TEST_FRAMEWORKS_ELEMENTS_WILDCARD_TESTS
-                            frameworks_retval=0
-#DEBUG DISABLE TESTING STOP
+                            $SST_PYTHON_APP_EXE $SST_TEST_FRAMEWORKS_ELEMENTS_APP_EXE -z -d -r $SST_MULTI_RANK_COUNT -t $SST_MULTI_THREAD_COUNT -w $SST_TEST_FRAMEWORKS_ELEMENTS_WILDCARD_TESTS
                             frameworks_retval=$?
                             echo "BAMBOO: SST Frameworks Elements Test retval = $frameworks_retval"
                         else
