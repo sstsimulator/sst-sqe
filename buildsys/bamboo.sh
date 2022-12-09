475c92345bbf3a4ce2395c3d3154bd73b02e4433
Gwen <grvosku@sandia.gov>
2022-12-09 11:17:07 -0700

    Update branch to target v12.1.0_beta

--- buildsys/bamboo.sh
+++ buildsys/bamboo.sh
@@ -2986,7 +2986,7 @@ fi
 
 # Which branches to use for each repo (default is devel)
 if [[ ${SST_SQEBRANCH:+isSet} != isSet ]] ; then
-    SST_SQEBRANCH=devel
+    SST_SQEBRANCH=v12.1.0_beta
     SST_SQEBRANCH="detached"
 else
     echo ' ' ;  echo ' ' ; echo ' ' ; echo ' '
@@ -2997,23 +2997,23 @@ else
 fi
 
 if [[ ${SST_COREBRANCH:+isSet} != isSet ]] ; then
-    SST_COREBRANCH=devel
+    SST_COREBRANCH=v12.1.0_beta
 fi
 
 if [[ ${SST_ELEMENTSBRANCH:+isSet} != isSet ]] ; then
-    SST_ELEMENTSBRANCH=devel
+    SST_ELEMENTSBRANCH=v12.1.0_beta
 fi
 
 if [[ ${SST_MACROBRANCH:+isSet} != isSet ]] ; then
-    SST_MACROBRANCH=devel
+    SST_MACROBRANCH=v12.1.0_beta
 fi
 
 if [[ ${SST_EXTERNALELEMENTBRANCH:+isSet} != isSet ]] ; then
-    SST_EXTERNALELEMENTBRANCH=master
+    SST_EXTERNALELEMENTBRANCH=v12.1.0_beta
 fi
 
 if [[ ${SST_JUNOBRANCH:+isSet} != isSet ]] ; then
-    SST_JUNOBRANCH=master
+    SST_JUNOBRANCH=v12.1.0_beta
 fi
 
 echo "#############################################################"
