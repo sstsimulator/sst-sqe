8784caa2150cbb1f03b467e94b74a7f0975137de
grvosku <grvosku@sandia.gov>
2023-04-17 11:52:55 -0600

    Bump pin version to 3.26

--- buildsys/bamboo.sh
+++ buildsys/bamboo.sh
@@ -2702,16 +2702,16 @@ else
                 echo "  PIN IS NOT ENABLED BY SST_WITHOUT_PIN flag"
             else
                 # Check that the default Intel PIN module is available
-                # For Linux = pin/pin-3.17-98314-g0c048d619-gcc-linux
+                # For Linux = pin/pin-3.26-98960-g1fc9d60e6-gcc-linux
                 #           ModuleEx puts the avail output on Stdout (where it belongs.)
-                ModuleEx avail | egrep -q "pin/pin-3.17"
+                ModuleEx avail | egrep -q "pin/pin-3.26"
                 if [ $? == 0 ]
                 then
-                # if `pin module is available, use pin/pin-3.17.
+                # if `pin module is available, use pin/pin-3.26.
                     if [ $kernel != "Darwin" ] ; then
-                        echo "USING INTEL PIN ENVIRONMENT MODULE pin-3.17-98314-g0c048d619-gcc-linux"
+                        echo "USING INTEL PIN ENVIRONMENT MODULE pin-3.26-98690-g1fc9d60e6-gcc-linux"
                         echo "LOADING INTEL PIN ENVIRONMENT MODULE"
-                        ModuleEx load pin/pin-3.17-98314-g0c048d619-gcc-linux
+                        ModuleEx load pin/pin-3.26-98690-g1fc9d60e6-gcc-linux
                         echo  $INTEL_PIN_DIRECTORY
                         ls $INTEL_PIN_DIRECTORY
                         export SST_USING_PIN3=1
