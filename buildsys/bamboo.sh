d1a59cce444a8a9c81e16eb2b5be62b9beca986e
Eric Berquist <ejberqu@sandia.gov>
2023-12-08 13:14:34 -0700

    Revert "remove unused SST_STOP_AFTER_BUILD"
    This reverts commit 1901e6a051a48a363dcd384749201a61a8f7de31.

--- buildsys/bamboo.sh
+++ buildsys/bamboo.sh
@@ -728,20 +728,22 @@ getconfig() {
 #   Return value:
 linuxSetMPI() {
 
-    # For some reason, .bashrc is not being run prior to
-    # this script. Kludge initialization of modules.
+   if [[ ${SST_STOP_AFTER_BUILD:+isSet} != isSet ]] ; then
+      # For some reason, .bashrc is not being run prior to
+      # this script. Kludge initialization of modules.
 
-    echo "Attempt to initialize the modules utility.  Look for modules init file in 1 of 2 places"
+      echo "Attempt to initialize the modules utility.  Look for modules init file in 1 of 2 places"
 
-    echo "Location 1: ls -l /etc/profile.modules"
-    echo "Location 2: ls -l /etc/profile.d/modules.sh"
-    if [ -r /etc/profile.modules ] ; then
-        source /etc/profile.modules
-        echo "bamboo.sh: loaded /etc/profile.modules"
-    elif [ -r /etc/profile.d/modules.sh ] ; then
-        source /etc/profile.d/modules.sh
-        echo "bamboo.sh: loaded /etc/profile.d/modules"
-    fi
+      echo "Location 1: ls -l /etc/profile.modules"
+      echo "Location 2: ls -l /etc/profile.d/modules.sh"
+      if [ -r /etc/profile.modules ] ; then
+          source /etc/profile.modules
+          echo "bamboo.sh: loaded /etc/profile.modules"
+      elif [ -r /etc/profile.d/modules.sh ] ; then
+          source /etc/profile.d/modules.sh
+          echo "bamboo.sh: loaded /etc/profile.d/modules"
+      fi
+   fi
 
    echo "Testing modules utility via ModuleEx..."
    echo "ModuleEx avail"
@@ -2025,12 +2027,14 @@ else
                 # Perform the build
                 dobuild -t $SST_BUILD_TYPE -a $arch -k $kernel
                 retval=$?
-                if [ $retval -eq 0 ] ; then
-                    echo "$0 : exit success."
-                else
-                    echo "$0 : exit failure."
+                if [[ ${SST_STOP_AFTER_BUILD:+isSet} == isSet ]] ; then
+                    if [ $retval -eq 0 ] ; then
+                        echo "$0 : exit success."
+                    else
+                        echo "$0 : exit failure."
+                    fi
+                    exit $retval
                 fi
-                exit $retval
             fi
 
             echo "PWD $LINENO = `pwd`"
