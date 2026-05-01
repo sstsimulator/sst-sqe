8ee0491c8c0a32193fabb5a5da6c25b0ea0b6f9b
Justin M. LaPre <jmlapre@sandia.gov>
2026-05-01 02:34:31 +0000

    Add debug.sh

---
+++ debug.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+set -x
+
+IFS=$'\n\t'
+SST_DEPS_USER_DIR=$(mktemp -d)
+GITHUB_WORKSPACE=$(mktemp -d)
+
+export SST_DEPS_USER_DIR
+export GITHUB_WORKSPACE
+export BUILD_TYPE=sstmainline_config_no_mpi
+export COMPILER_TYPE=none
+export MPI_TYPE=none
+export CUDA_TYPE=none
+
+# shellcheck disable=SC1091
+source .github/scripts/call_bamboo.sh
