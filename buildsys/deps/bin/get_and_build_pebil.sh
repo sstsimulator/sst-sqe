a6b388f82a4a92b5703e0ba9c05dddf1b4a97df4
Eric Berquist <ejberqu@sandia.gov>
2026-06-09 16:52:04 -0600

    Ensure compilers are those from SST configuration

--- buildsys/deps/bin/get_and_build_pebil.sh
+++ buildsys/deps/bin/get_and_build_pebil.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # get_and_build_pebil.sh: Clone and install
 # https://github.com/epanalytics/PEBIL/tree/feature/sst
@@ -16,10 +16,17 @@ perl -i'' -pe 's{git\@github\.com:}{https://github.com/}g' .gitmodules
 git submodule init external/epa-inst-libs
 git submodule update
 SST_CONFIG="${SST_CORE_INSTALL_BIN}"/sst-config
-./configure \
-    --with-sst-core="$("${SST_CONFIG}" --prefix)" \
-    --with-sst-elements="$("${SST_CONFIG}" SST_ELEMENT_LIBRARY SST_ELEMENT_LIBRARY_SOURCE_ROOT)"
+loc_sst_core_install="$("${SST_CONFIG}" --prefix)"
+loc_sst_elements_source="$("${SST_CONFIG}" SST_ELEMENT_LIBRARY SST_ELEMENT_LIBRARY_SOURCE_ROOT)"
+CC="$("${SST_CONFIG}" --CC)" \
+  CXX="$("${SST_CONFIG}" --CXX)" \
+  MPICC="$("${SST_CONFIG}" --MPICC)" \
+  MPICXX="$("${SST_CONFIG}" --MPICXX)" \
+  MPI_CPPFLAGS="$("${SST_CONFIG}" --MPI_CPPFLAGS)"\
+  ./configure \
+  --with-sst-core="${loc_sst_core_install}" \
+  --with-sst-elements="${loc_sst_elements_source}"
 # shellcheck disable=SC1091
 source "${SST_DEPS_SRC_STAGED_PEBIL}"/bashrc
-make all install
+make all -j"$(nproc)"
 popd
